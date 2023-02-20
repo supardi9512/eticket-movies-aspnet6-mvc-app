53a18cc2dbfd99e2e0b358a80b6a9ec950d51587
supardi_9512 <supardi@atomic.id>
2023-02-20 17:10:45 +0700

    model and db context

---
+++ eTickets/Data/AppDbContext.cs
@@ -0,0 +1,39 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace eTickets.Data
+{
+    public class AppDbContext:DbContext
+    {
+        //CONSTRUCTOR
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        //MODEL CREATING
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Actor_Movie>().HasKey(am => new
+            {
+                am.ActorId,
+                am.MovieId,
+            });
+
+            //DEFINE JOINING TABLE ACTOR_MOVIE
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Movie).WithMany(am => am.Actors_Movies).HasForeignKey(m =>
+             m.MovieId);
+            modelBuilder.Entity<Actor_Movie>().HasOne(m => m.Actor).WithMany(am => am.Actors_Movies).HasForeignKey(m =>
+             m.ActorId);
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        //DEFINE THE TABLE NAME
+        public DbSet<Actor> Actors { get; set; }
+        public DbSet<Movie> Movies { get; set; }
+        public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Producer> Producers { get; set; }
+
+    }
+}
