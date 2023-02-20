c3272e65cde32944bf69ad9d2e097782475c7dd8
supardi_9512 <supardi@atomic.id>
2023-02-20 17:17:00 +0700

    update db context

--- eTickets/Data/AppDbContext.cs
+++ eTickets/Data/AppDbContext.cs
@@ -33,6 +33,7 @@ namespace eTickets.Data
         public DbSet<Actor> Actors { get; set; }
         public DbSet<Movie> Movies { get; set; }
         public DbSet<Actor_Movie> Actors_Movies { get; set; }
+        public DbSet<Cinema> Cinemas { get; set; }
         public DbSet<Producer> Producers { get; set; }
 
     }
