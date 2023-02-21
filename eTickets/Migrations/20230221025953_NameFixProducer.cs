854b6d4b877d08b88553e04a0fd12dbdb05d6470
supardi_9512 <supardi@atomic.id>
2023-02-21 11:33:06 +0700

    database migration

---
+++ eTickets/Migrations/20230221025953_NameFixProducer.cs
@@ -0,0 +1,68 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace eTickets.Migrations
+{
+    /// <inheritdoc />
+    public partial class NameFixProducer : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Producers_ProducerId",
+                table: "Movies");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Producers",
+                table: "Producers");
+
+            migrationBuilder.RenameTable(
+                name: "Producers",
+                newName: "Producer");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Producer",
+                table: "Producer",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Producer_ProducerId",
+                table: "Movies",
+                column: "ProducerId",
+                principalTable: "Producer",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Movies_Producer_ProducerId",
+                table: "Movies");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Producer",
+                table: "Producer");
+
+            migrationBuilder.RenameTable(
+                name: "Producer",
+                newName: "Producers");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Producers",
+                table: "Producers",
+                column: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Movies_Producers_ProducerId",
+                table: "Movies",
+                column: "ProducerId",
+                principalTable: "Producers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
