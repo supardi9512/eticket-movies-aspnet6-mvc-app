14f9e7daf939e3fb538df38b7a3a89ae44232c6e
supardi_9512 <supardi@atomic.id>
2023-02-22 11:04:59 +0700

    add iactors service interface

---
+++ eTickets/Data/Services/IActorsService.cs
@@ -0,0 +1,23 @@
+﻿using eTickets.Models;
+using Microsoft.EntityFrameworkCore.Update.Internal;
+
+namespace eTickets.Data.Services
+{
+    public interface IActorsService
+    {
+        //GET ALL DATA
+        IEnumerable<Actor> GetAll();
+
+        //GET BY ID
+        Actor GetById(int id);
+
+        //CREATE DATA
+        void Add(Actor actor);
+
+        //UPDATE DATA
+        Actor Update(int id, Actor newActor);
+
+        //DELETE DATA
+        void Delete(int id);
+    }
+}
